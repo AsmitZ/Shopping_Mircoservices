e982a5f7ea4066dd2e7713811a924c2e3adf33cb
asmit-goel <asmitgoel1999@gmail.com>
2024-10-13 00:16:14 +0530

    Added controller with create order action
    Signed-off-by: asmit-goel <asmitgoel1999@gmail.com>

---
+++ Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -0,0 +1,59 @@
+using AutoMapper;
+using Mango.Services.OrderAPI.Data;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dto;
+using Mango.Services.OrderAPI.Services.IServices;
+using Mango.Services.OrderAPI.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.OrderAPI.Controllers;
+
+[Route("api/order")]
+public class OrderAPIController : ControllerBase
+{
+    private readonly AppDbContext _dbContext;
+    private readonly IProductService _productService;
+    private readonly IMapper _mapper;
+    private ResponseDto _response;
+
+    public OrderAPIController(AppDbContext dbContext, IProductService productService, IMapper mapper)
+    {
+        ArgumentNullException.ThrowIfNull(dbContext);
+        ArgumentNullException.ThrowIfNull(productService);
+        ArgumentNullException.ThrowIfNull(mapper);
+
+        _dbContext = dbContext;
+        _productService = productService;
+        _mapper = mapper;
+        _response = new ResponseDto();
+    }
+
+    [Authorize]
+    [HttpPost("CreateOrder")]
+    public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
+    {
+        try
+        {
+            var orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+            orderHeaderDto.UserId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+            orderHeaderDto.Status = SD.Status.Pending;
+            orderHeaderDto.OrderTime = DateTime.Now;
+            orderHeaderDto.OrderDetails = _mapper.Map<List<OrderDetailsDto>>(cartDto.CartDetails);
+
+            var createdOrder = _dbContext.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+            _dbContext.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto));
+            await _dbContext.SaveChangesAsync();
+
+            orderHeaderDto.OrderHeaderId = createdOrder.OrderHeaderId;
+            _response.Result = orderHeaderDto;
+        }
+        catch (Exception e)
+        {
+            _response.IsSuccess = false;
+            _response.Message = e.Message;
+        }
+
+        return _response;
+    }
+}
