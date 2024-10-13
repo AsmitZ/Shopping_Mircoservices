febbebf09399db50e1c43916c171d3f624588efc
asmit-goel <asmitgoel1999@gmail.com>
2024-10-13 19:37:34 +0530

    Added gateway solution with ocelot to protect internal APIs
    Signed-off-by: asmit-goel <asmitgoel1999@gmail.com>

---
+++ GatewaySolution/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,33 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GatewaySolution.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddAppAuthentication(this WebApplicationBuilder builder)
+    {
+        var secret = builder.Configuration.GetValue<string>("AppSettings:Secret");
+        var issuer = builder.Configuration.GetValue<string>("AppSettings:Issuer");
+        var audience = builder.Configuration.GetValue<string>("AppSettings:Audience");
+        var key = Encoding.ASCII.GetBytes(secret);
+
+        builder.Services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = audience,
+                ValidIssuer = issuer
+            };
+        });
+    }
+}
